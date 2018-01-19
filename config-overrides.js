5eaeccc4879dae98130e64dbce5c8f22443fe3ff
David Pordomingo <David.Pordomingo.F@gmail.com>
2018-01-19 08:59:31 +0100

    Adding sass support

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,21 @@
 const { compose } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
-module.exports = compose(rewireLess);
+const rewireSass = function(config, env) {
+  const oneOfRules = config.module.rules.find(r => r.oneOf)
+  if (oneOfRules) {
+    // Exclude sass files from the file-loader
+    const fileLoaderRule = oneOfRules.oneOf.find(r => r.loader && r.loader.indexOf("file-loader") > -1);
+    fileLoaderRule && fileLoaderRule.exclude.push(/\.scss$/);
+
+    // Add a new rule to process sass
+    oneOfRules.oneOf.push({
+      test: /\.scss$/,
+      loader: ["style-loader", "css-loader", "sass-loader"]
+    });
+  }
+
+  return config;
+}
+
+module.exports = compose(rewireLess, rewireSass);
